c603608b43f3e0609f7d44bc242978ca2f00b687
ZhandosXIX <ospanbekov.k@mail.ru>
2024-02-25 16:42:46 +0600

    first commit

--- OzinsheDemo/Controllers/FavoriteTableViewController.swift
+++ OzinsheDemo/Controllers/FavoriteTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class FavoriteTableViewController: UITableViewController {
 
+    var arrayFavorite = ["Image1","Image2","Image3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,23 +25,26 @@ class FavoriteTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return arrayFavorite.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MovieTableViewCell
 
         // Configure the cell...
+        cell.setData(movie: arrayFavorite[indexPath.row])
 
         return cell
     }
-    */
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 153
+    }
 
     /*
     // Override to support conditional editing of the table view.
--- OzinsheDemo/Controllers/MovieTableViewCell.swift
+++ OzinsheDemo/Controllers/MovieTableViewCell.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var Name: UILabel!
+    
+    @IBOutlet weak var year: UILabel!
+    
 
+    @IBOutlet weak var playView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        posterImageView.layer.cornerRadius = 8
+        playView.layer.cornerRadius = 8
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +31,7 @@ class MovieTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    func setData(movie: String){
+        posterImageView.image = UIImage(named: movie)
+    }
 }
